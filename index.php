e5b7f683357c0656f08465bd253b695a839df1bc
sgehin <sfa.gehin@gmail.com>
2017-11-19 17:45:17 +0100

    set cookie

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
+<?php setcookie('username','Hanna'); 
+      setcookie('username','Bas',time()+ 60 * 60 * 24 * 30,'/'); // expire date
+?>
+
 <html>
     <head>
         <meta charset="UTF-8">
